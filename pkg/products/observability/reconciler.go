bb0055469b0a559ca354bfcf1450ef5e729300b0
Valery Mogilevsky <vmogilev@redhat.com>
2021-09-05 13:29:37 +0300

    add alerts: observability/prometheusRules.go

--- pkg/products/observability/reconciler.go
+++ pkg/products/observability/reconciler.go
@@ -145,6 +145,13 @@ func (r *Reconciler) Reconcile(ctx context.Context, installation *integreatlyv1a
 		return phase, err
 	}
 
+	phase, err = r.newAlertsReconciler(r.log, r.installation.Spec.Type).ReconcileAlerts(ctx, client)
+	r.log.Infof("reconcilePrometheusRule", l.Fields{"phase": phase})
+	if err != nil || phase != integreatlyv1alpha1.PhaseCompleted {
+		events.HandleError(r.recorder, installation, phase, "Failed to reconcile alerts", err)
+		return phase, err
+	}
+
 	if string(r.Config.GetProductVersion()) != string(integreatlyv1alpha1.VersionObservability) {
 		r.Config.SetProductVersion(string(integreatlyv1alpha1.VersionObservability))
 		err := r.ConfigManager.WriteConfig(r.Config)
